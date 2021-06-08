a33147e04b3eb30d967fb1fad4ee5c55a9e94548
GifinoThoriq <66865274+GifinoThoriq@users.noreply.github.com>
2021-06-08 19:35:33 +0700

    adding the project

---
+++ script.js
@@ -0,0 +1,37 @@
+let slideIndex = 1;
+const slides = document.getElementsByClassName("carouselItem");
+const dots = document.getElementsByClassName("dot");
+totalSlide = slides.length
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  if (n > slides.length) {
+      slideIndex = 1
+    }
+
+  if (n < 1) {
+      slideIndex = totalSlide
+    }
+
+  for (let i = 0; i < totalSlide; i++) {
+      slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//form
